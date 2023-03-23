76d0c8ecaa58ecb2cad7cbab86b543679e7d88f8
Gustavo Alzuru <gustavoalzuru7@gmail.com>
2023-03-23 16:10:35 -0400

    adding features to the game

--- Projects/02-Secure-Vault/script.js
+++ Projects/02-Secure-Vault/script.js
@@ -1,11 +1,50 @@
-// assign a constant variable a <p> tag with the class 
+// Assign my variables to the DOM element I need
 const codeText = document.querySelector('.code-text')
+const fComb = document.querySelector('.firstCode')
+const sComb = document.querySelector('.secondCode')
+const tComb = document.querySelector('.thirdCode')
+const form = document.querySelector('.form')
+const textSmall = 'You need a smaller number'
+const textBig = 'You need a bigger number'
+let openVault = document.querySelector('.open-vault')
 
-// three variables with different operators to get a unique combination
-const firstCombination = 2 * 5
-const secCombination = 80 / 2
-const thirdCombination = 10 + 30 - 1
-
-const codePhrase = `You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${firstCombination} - ${secCombination} - ${thirdCombination}`
-
-codeText.textContent = codePhrase
+// Getting the three numbers the users put in the form with a formData
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    codeText.textContent = ''
+    fComb.textContent = ''
+    sComb.textContent = ''
+    tComb.textContent = ''
+    openVault.style.display = 'none'
+    const formData = new FormData(form)
+    const first = formData.get('code-1')
+    const second = formData.get('code-2')
+    const third = formData.get('code-3')
+    if(!first || !second || !third){
+        codeText.textContent = 'You have an empty space';
+        return
+    }
+    if(first == 6 && second == 2 && third == 9){
+        codeText.textContent = 'You get the exact combination now you can open the vault'
+        openVault.style.display = 'inline'
+        return
+    }
+    if(first > 6){
+        fComb.textContent = textSmall
+    }
+    if(first < 6){
+        fComb.textContent = textBig
+    }
+    if(second > 2){
+        sComb.textContent = textSmall
+    }
+    if(second < 2){
+        sComb.textContent = textBig
+    }
+    if(third > 9){
+        tComb.textContent = textSmall
+    }
+    if(third < 9){
+        tComb.textContent = textBig
+    }
+})
