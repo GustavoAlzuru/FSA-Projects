ea8726ee127bb1d0f9471a17d63107e007cbaaec
Gustavo Alzuru <gustavoalzuru7@gmail.com>
2023-04-21 19:02:41 -0400

    objects workshop

---
+++ Projects/05-Objects-Workshop/froyo-survey.js
@@ -0,0 +1,31 @@
+// create an object customer ✅
+// modify email phone zipcode favoriteFlavors using bracket notation ✅
+// delete zipCode and favoriteStore ✅
+// add new keys and values ✅
+// in array print the keys ✅
+
+const customer = {
+    firstName: 'Jake',
+    lastName: 'smith',
+    email: 'jaekSmih!aol.com',
+    phone: undefined,
+    zipCode: '631',
+    favoriteFlavors: 32,
+    cupSize: 'medium',
+    favoriteStore: 'Target',
+    firstVisit: false
+}
+
+customer['email'] = 'Jak3Smith1992@email.com'
+customer['phone'] = 3195551234
+customer['zipCode'] = "63132"
+customer['favoriteFlavors'] = ['coffee', 'strawberry', 'matcha']
+
+delete customer.zipCode
+delete customer.favoriteStore
+
+customer.toppings = ['chocolate sprinkles', 'wafer straws', 'gummy bears']
+customer.futureFlavors = 'mango'
+customer.todaysPurchaseCost = 5.32
+
+console.log(Object.keys(customer))
